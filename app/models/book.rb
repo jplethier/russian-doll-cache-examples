1b08e522bc134f9b3440ca292546b783ffb19f1a
Joao Paulo Lethier <jplethier@gmail.com>
2021-01-31 15:34:05 -0300

    relationship between books, genres and publishers

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :author
+  belongs_to :publisher
+  belongs_to :genre
 
   validates :title, :image_url, presence: true
 end
--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,2 +1,4 @@
 class Publisher < ApplicationRecord
+  has_many :books
+  has_many :authors, through: :books
 end
