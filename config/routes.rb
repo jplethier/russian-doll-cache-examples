4f8509a049adccfcc534cd9e030314ffaa2512e7
Joao Paulo Lethier <jplethier@gmail.com>
2021-01-31 14:31:40 -0300

    creates controllers for books and authors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books, only: %i[show index]
+  resources :authors, only: %i[show index]
 end
---
+++ test/controllers/book_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class BookControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get book_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get book_show_url
+    assert_response :success
+  end
+
+end
