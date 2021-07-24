c70b16e66cd607aadbf9121fa974344de22c351b
Joao Paulo Lethier <jplethier@gmail.com>
2021-07-24 17:09:29 -0300

    adds low level caching example on authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,5 +5,11 @@ class AuthorsController < ApplicationController
 
   def show
     @author = Author.find(params[:id])
+    @stats = Rails.cache.fetch(["stats", @author]) do
+      { total_books: @author.books.count,
+        total_pages_written: @author.books.sum(:num_pages),
+        average_rating: @author.books.average(:rating),
+        genres_count: @author.books.count("distinct(genre_id)") }
+    end
   end
 end
